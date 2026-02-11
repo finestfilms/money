9203bcf5686bc3beeee051a17bbfdb13f8aa35fd
finestfilms <finest.films.money@gmail.com>
2026-02-11 16:01:12 +1100

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function drawFrame(index) {
   const overflowY = drawHeight - canvasHeight;
   
   // Shift down 40% of the overflow
-  const y = -(overflowY * 0.1);
+  const y = -(overflowY * 0.25);
 
   ctx.drawImage(img, x, y, drawWidth, drawHeight);
 }
